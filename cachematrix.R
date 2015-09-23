dea21c20e004f6a79c8c6ac47f205c3a522bcbee
Dominik Rusiecki <dominik.rusiecki.krakow@gmail.com>
2015-09-23 23:30:24 +0200

    Solve it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are designed to create a structure holding a matrix
+## and to query it in an efficient way i.e. using cache
 
-## Write a short comment describing this function
 
+## creates a structure holding the matrix
+## the matrix has methods get and set to read from/write to the matrix itself
+## as well as getInv and setInv to read from/write to the cache holding the inverse
+## of that matrix
+## updates of the matrix always clear the cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  getInv <- function() inv
+  setInv <- function(i) inv <<- i
+  list(get = get, set = set, getInv = getInv, setInv = setInv)
 }
 
-
-## Write a short comment describing this function
-
+## queries a structure created using makeCacheMatrix for its inverse
+## if the cache is available, its value is returned
+## otherwise the new inverse is computed, put into the cache
+## and returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(is.null(x$getInv())) {
+    inverse <- solve(x$get(), ...)
+    x$setInv(inverse)
+    inverse
+  } else {
+    message("Returning the cached value")
+    x$getInv()
+  }
 }
